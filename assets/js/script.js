84ca0facbfef4aa2c1939940b32d869b07f22c21
Jacob Canepa <jacobmcanepa@gmail.com>
2022-08-09 01:40:43 -0700

    added 5 day forecast js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var dt = DateTime.now();
 var dtISO = dt.toString();
 var formEl = document.querySelector("#form");
 var locationInputEl = document.querySelector("#location");
-var infoSectionEl = document.querySelector("#info-section");
+var topContainerEl = document.querySelector("#top");
+var bottomContainerEl = document.querySelector("#bottom");
 
 
 var getLocationKey = function(location) {
@@ -15,9 +16,10 @@ var getLocationKey = function(location) {
         if (data.length === 0) {
           alert("Error: City not found");
         } else {
-          infoSectionEl.innerHTML = "";
+          topContainerEl.innerHTML = "";
+          bottomContainerEl.innerHTML = "";
+          getForecast(data[0].lat, data[0].lon);
           getCurrentWeather(data[0].lat, data[0].lon);
-          //getForecast(data[0].lat, data[0].lon);
         }
       });
     }
@@ -54,16 +56,14 @@ var getCurrentWeather = function(lat, lon) {
   fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
       console.log(data);
-
-      var containerEl = document.createElement("div");
-      containerEl.classList = "container mt-3";
-    
+      var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
       var rowEl = document.createElement("div");
       rowEl.classList = "row border border-dark w-98";
     
       var colEl = document.createElement("div");
       colEl.className = "col-12";
-      colEl.innerHTML = "<h2 class='mt-1 font-weight-bold'>" + data.name + " (" + dt.month + "/" + dt.day + "/" + dt.year + ")</h2>";
+      colEl.innerHTML = "<h2 class='mt-1 font-weight-bold'>"
+         + data.name + " (" + dt.month + "/" + dt.day + "/" + dt.year + ")<img src='" + icon + "' alt='weather-icon' /></h2>";
     
       var listEl = document.createElement("ul");
       listEl.setAttribute("style", "list-style: none;");
@@ -86,7 +86,6 @@ var getCurrentWeather = function(lat, lon) {
 
       fetch(uvUrl, {headers: {"x-access-token": uvApiKey}}).then(function(response) {
         response.json().then(function(data) {
-          console.log(data);
           var uvIndexEl = document.createElement("li");
           uvIndexEl.classList = "pb-2 info-list-top";
 
@@ -123,8 +122,7 @@ var getCurrentWeather = function(lat, lon) {
 
       colEl.appendChild(listEl);
       rowEl.appendChild(colEl);
-      containerEl.appendChild(rowEl);
-      infoSectionEl.appendChild(containerEl);
+      topContainerEl.appendChild(rowEl);
     });
   })
   .catch(function(error) {
@@ -133,11 +131,176 @@ var getCurrentWeather = function(lat, lon) {
 };
 
 var getForecast = function(lat, lon) {
-  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=a97605dca80be3bef695a54ff827f901";
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=a97605dca80be3bef695a54ff827f901";
 
   fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
-      console.log(data);
+      bottomContainerEl.innerHTML = "<h3>5 Day Forecast:</h3>";
+
+      var rowEl = document.createElement("div");
+      rowEl.className = "row"
+
+      // day one forecast
+      var dayOneDiv = document.createElement("div");
+      dayOneDiv.classList = "col mx-1 text-light bg-primary";
+
+      var dayOneList = document.createElement("ul");
+      dayOneList.setAttribute("style", "list-style: none;");
+
+      var dayOneDate = document.createElement("li");
+      dayOneDate.innerHTML = "(" + dt.month + "/" + (dt.day + 1) + "/" + dt.year + ")";
+      dayOneList.appendChild(dayOneDate);
+
+      var dayOneIcon = document.createElement("li");
+      var icon1 ="http://openweathermap.org/img/wn/" + data.list[6].weather[0].icon + ".png";
+      dayOneIcon.innerHTML = "<img src='" + icon1 + "' alt='icon' />";
+      dayOneList.appendChild(dayOneIcon);
+
+      var dayOneTemp = document.createElement("li");
+      dayOneTemp.innerHTML = "Temp: " + Math.floor(data.list[6].main.temp) + "F";
+      dayOneList.appendChild(dayOneTemp);
+
+      var dayOneWind = document.createElement("li");
+      dayOneWind.innerHTML = "Wind: " + data.list[6].wind.speed + " MPH";
+      dayOneList.appendChild(dayOneWind);
+
+      var dayOneHumidity = document.createElement("li");
+      dayOneHumidity.innerHTML = "Humidity: " + data.list[6].main.humidity + "%";
+      dayOneList.appendChild(dayOneHumidity);
+ 
+      dayOneDiv.appendChild(dayOneList);
+      rowEl.appendChild(dayOneDiv);
+      // end day one forecast
+
+      // day two forecast
+      var dayTwoDiv = document.createElement("div");
+      dayTwoDiv.classList = "col mx-1 text-light bg-primary";
+
+      var dayTwoList = document.createElement("ul");
+      dayTwoList.setAttribute("style", "list-style: none;");
+
+      var dayTwoDate = document.createElement("li");
+      dayTwoDate.innerHTML = "(" + dt.month + "/" + (dt.day + 2) + "/" + dt.year + ")";
+      dayTwoList.appendChild(dayTwoDate);
+
+      var dayTwoIcon = document.createElement("li");
+      var icon2 ="http://openweathermap.org/img/wn/" + data.list[14].weather[0].icon + ".png";
+      dayTwoIcon.innerHTML = "<img src='" + icon2 + "' alt='icon' />";
+      dayTwoList.appendChild(dayTwoIcon);
+
+      var dayTwoTemp = document.createElement("li");
+      dayTwoTemp.innerHTML = "Temp: " + Math.floor(data.list[14].main.temp) + "F";
+      dayTwoList.appendChild(dayTwoTemp);
+
+      var dayTwoWind = document.createElement("li");
+      dayTwoWind.innerHTML = "Wind: " + data.list[14].wind.speed + " MPH";
+      dayTwoList.appendChild(dayTwoWind);
+
+      var dayTwoHumidity = document.createElement("li");
+      dayTwoHumidity.innerHTML = "Humidity: " + data.list[14].main.humidity + "%";
+      dayTwoList.appendChild(dayTwoHumidity);
+ 
+      dayTwoDiv.appendChild(dayTwoList);
+      rowEl.appendChild(dayTwoDiv);
+      // end day two forecast
+
+      // day three forecast
+      var dayThreeDiv = document.createElement("div");
+      dayThreeDiv.classList = "col mx-1 text-light bg-primary";
+
+      var dayThreeList = document.createElement("ul");
+      dayThreeList.setAttribute("style", "list-style: none;");
+
+      var dayThreeDate = document.createElement("li");
+      dayThreeDate.innerHTML = "(" + dt.month + "/" + (dt.day + 3) + "/" + dt.year + ")";
+      dayThreeList.appendChild(dayThreeDate);
+
+      var dayThreeIcon = document.createElement("li");
+      var icon3 ="http://openweathermap.org/img/wn/" + data.list[22].weather[0].icon + ".png";
+      dayThreeIcon.innerHTML = "<img src='" + icon3 + "' alt='icon' />";
+      dayThreeList.appendChild(dayThreeIcon);
+
+      var dayThreeTemp = document.createElement("li");
+      dayThreeTemp.innerHTML = "Temp: " + Math.floor(data.list[22].main.temp) + "F";
+      dayThreeList.appendChild(dayThreeTemp);
+
+      var dayThreeWind = document.createElement("li");
+      dayThreeWind.innerHTML = "Wind: " + data.list[22].wind.speed + " MPH";
+      dayThreeList.appendChild(dayThreeWind);
+
+      var dayThreeHumidity = document.createElement("li");
+      dayThreeHumidity.innerHTML = "Humidity: " + data.list[22].main.humidity + "%";
+      dayThreeList.appendChild(dayThreeHumidity);
+ 
+      dayThreeDiv.appendChild(dayThreeList);
+      rowEl.appendChild(dayThreeDiv);
+      // end day three forecast
+
+      // day four forecast
+      var dayFourDiv = document.createElement("div");
+      dayFourDiv.classList = "col mx-1 text-light bg-primary";
+
+      var dayFourList = document.createElement("ul");
+      dayFourList.setAttribute("style", "list-style: none;");
+
+      var dayFourDate = document.createElement("li");
+      dayFourDate.innerHTML = "(" + dt.month + "/" + (dt.day + 4) + "/" + dt.year + ")";
+      dayFourList.appendChild(dayFourDate);
+
+      var dayFourIcon = document.createElement("li");
+      var icon4 ="http://openweathermap.org/img/wn/" + data.list[30].weather[0].icon + ".png";
+      dayFourIcon.innerHTML = "<img src='" + icon4 + "' alt='icon' />";
+      dayFourList.appendChild(dayFourIcon);
+
+      var dayFourTemp = document.createElement("li");
+      dayFourTemp.innerHTML = "Temp: " + Math.floor(data.list[30].main.temp) + "F";
+      dayFourList.appendChild(dayFourTemp);
+
+      var dayFourWind = document.createElement("li");
+      dayFourWind.innerHTML = "Wind: " + data.list[30].wind.speed + " MPH";
+      dayFourList.appendChild(dayFourWind);
+
+      var dayFourHumidity = document.createElement("li");
+      dayFourHumidity.innerHTML = "Humidity: " + data.list[30].main.humidity + "%";
+      dayFourList.appendChild(dayFourHumidity);
+ 
+      dayFourDiv.appendChild(dayFourList);
+      rowEl.appendChild(dayFourDiv);
+      // end day four forecast
+
+      // day five forecast
+      var dayFiveDiv = document.createElement("div");
+      dayFiveDiv.classList = "col mx-1 text-light bg-primary";
+
+      var dayFiveList = document.createElement("ul");
+      dayFiveList.setAttribute("style", "list-style: none;");
+
+      var dayFiveDate = document.createElement("li");
+      dayFiveDate.innerHTML = "(" + dt.month + "/" + (dt.day + 5) + "/" + dt.year + ")";
+      dayFiveList.appendChild(dayFiveDate);
+
+      var dayFiveIcon = document.createElement("li");
+      var icon5 ="http://openweathermap.org/img/wn/" + data.list[38].weather[0].icon + ".png";
+      dayFiveIcon.innerHTML = "<img src='" + icon5 + "' alt='icon' />";
+      dayFiveList.appendChild(dayFiveIcon);
+
+      var dayFiveTemp = document.createElement("li");
+      dayFiveTemp.innerHTML = "Temp: " + Math.floor(data.list[38].main.temp) + "F";
+      dayFiveList.appendChild(dayFiveTemp);
+
+      var dayFiveWind = document.createElement("li");
+      dayFiveWind.innerHTML = "Wind: " + data.list[38].wind.speed + " MPH";
+      dayFiveList.appendChild(dayFiveWind);
+
+      var dayFiveHumidity = document.createElement("li");
+      dayFiveHumidity.innerHTML = "Humidity: " + data.list[38].main.humidity + "%";
+      dayFiveList.appendChild(dayFiveHumidity);
+ 
+      dayFiveDiv.appendChild(dayFiveList);
+      rowEl.appendChild(dayFiveDiv);
+      // endday five forecast
+
+      bottomContainerEl.appendChild(rowEl);
     });
   })
   .catch(function(error) {
